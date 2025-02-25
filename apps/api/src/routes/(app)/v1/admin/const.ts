e886a82765ae3910376a7fd38959ffcb17f116fe
JoshuaZiegler <joshua.ziegler@titanom.com>
2025-02-25 16:11:22 +0100

    feat: add initial calculation of cost reports

--- apps/api/src/routes/(app)/v1/admin/const.ts
+++ apps/api/src/routes/(app)/v1/admin/const.ts
@@ -2,6 +2,7 @@ import { handler as v1_admin_organizations_getHandler } from "./organizations/ge
 import { handler as v1_admin_apiKey_postHandler } from "./api-key/post";
 import { handler as v1_admin_organizations_$organizationId_$projectId_getHandler } from "./organizations/$organizationId/$projectId/get";
 import { handler as v1_admin_organizations_$organizationId_getHandler } from "./organizations/$organizationId/get";
+import { handler as v1_admin_organization_$organizationId_report_getHandler } from "./organizations/$organizationId/report/get";
 import { RouteHandlerDefinition } from "@/handlers";
 
 export const adminRouteHandlerDefinitions: Array<RouteHandlerDefinition> = [
@@ -24,5 +25,10 @@ export const adminRouteHandlerDefinitions: Array<RouteHandlerDefinition> = [
     path: "/v1/admin/api-key",
     method: "POST",
     handler: v1_admin_apiKey_postHandler,
+  },
+  {
+    path: "/v1/admin/organizations/:organizationId/report/:year",
+    method: "GET",
+    handler: v1_admin_organization_$organizationId_report_getHandler,
   },
 ];
---
+++ apps/api/src/routes/(app)/v1/admin/organizations/$organizationId/report/get.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { validateAdminApiKey } from "../../../utils";
+import { dbGetProjectsWithApiKeys } from "@dgpt/db";
+import z from "zod";
+import { convertToCSV, createMonthlyCostReports } from "./utils";
+
+export async function handler(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const validationResult = validateAdminApiKey(request, reply);
+
+  if (!validationResult.isValid) return;
+
+  const { organizationId, year } = z
+    .object({
+      organizationId: z.string(),
+      year: z.coerce.number().default(2025),
+    })
+    .parse(request.params);
+
+  const { format } = z
+    .object({ format: z.enum(["csv", "json"]).default("json") })
+    .parse(request.query);
+
+  const projects = await dbGetProjectsWithApiKeys({ organizationId });
+
+  const report = await createMonthlyCostReports({ projects, year });
+
+  if (format === "csv") {
+    const csvString = convertToCSV(report);
+    reply.send(csvString).status(200);
+    return;
+  }
+
+  return reply.send({ report }).status(200);
+}

fc9d6df10f6317df3deff4120c8181edf68793c9
nachthammer <alexandervonberkov1@gmail.com>
2025-02-26 18:55:42 +0100

    feat: add sentry

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -4,4 +4,8 @@ export const env = {
   apiBaseUrl: process.env.API_BASE_URL ?? "http://127.0.0.1:3002",
   apiName: process.env.API_NAME ?? "DeutschlandGPT",
   apiKey: getDefinedOrThrow(process.env.API_KEY, "process.env.API_KEY"),
+  sentryDsn: getDefinedOrThrow(
+    process.env.SENTRY_DSN,
+    "process.env.SENTRY_DSN",
+  ),
 };

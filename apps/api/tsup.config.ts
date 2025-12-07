cba8ff68de61d7af5f60a411981a2de0247675af
Tobias Wallura <tobias.wallura@xitaso.com>
2025-12-08 00:07:40 +0100

    TD-745 setup sentry upload with sourcemaps

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tsup";
+import { sentryEsbuildPlugin } from "@sentry/esbuild-plugin";
 
 export default defineConfig({
   entry: ["src/index.ts"],
@@ -7,4 +8,16 @@ export default defineConfig({
   clean: true,
   format: "cjs",
   noExternal: [/@dgpt\//],
+  esbuildPlugins: [
+    sentryEsbuildPlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+      debug: true,
+      sourcemaps: {
+        assets: "./dist/*",
+      },
+    }),
+  ],
 });
